bdf1bc46c6ddb1314e0bbda68cd32fb6345ba1ce
scantarbian <davidpamereanb@gmail.com>
2021-01-07 23:17:34 +0700

    finally, parallax

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ParallaxModule , ParallaxConfig  } from 'ngx-parallax';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeCoreComponent } from './home-core/home-core.component'
 import { WelcomeComponent } from './welcome/welcome.component';
 import { ThingsToDoComponent } from './things-to-do/things-to-do.component';
 import { QuickLinksComponent } from './quick-links/quick-links.component';
-import { FeaturedComponent } from './featured/featured.component'
+import { FeaturedComponent } from './featured/featured.component';
+
 
 
 @NgModule({
   declarations: [HomeCoreComponent, WelcomeComponent, ThingsToDoComponent, QuickLinksComponent, FeaturedComponent ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    ParallaxModule,
   ]
 })
 
